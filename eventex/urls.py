c336a6f96cebd01f70931359de7987eeaee35598
wellington-carvalho <wellington.carvalho@armazemparaiba.com.br>
2018-07-13 13:27:57 -0300

    Tela de Incrição

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import eventex.core.views
+from eventex.subscriptions.views import subscribe
 
 urlpatterns = [
     #path(r'^$', eventex.core.views.home),
     path('', eventex.core.views.home),
+    path('inscricao/', subscribe),
     path('admin/', admin.site.urls),
+
 ]
