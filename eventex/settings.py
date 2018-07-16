ad528b6c71f1e5055efba06176d40cd551f03359
wellington-carvalho <wellington.carvalho@armazemparaiba.com.br>
2018-07-16 16:13:32 -0300

    Aleracao no setting.py para enviar email de erro

--- eventex/settings.py
+++ eventex/settings.py
@@ -31,6 +31,10 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
+ADMINS=[
+    ('wellington', 'wellington.carvalho@armazemparaiba.com.br')
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -130,5 +134,6 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 
 
