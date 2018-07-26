912ece036098f333a992559c9f7748b51d7c6560
wellington-carvalho <wellington.carvalho@armazemparaiba.com.br>
2018-07-26 17:51:11 -0300

    Admin magic

--- eventex/settings.py
+++ eventex/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'test_without_migrations',
     'eventex.core',
-    'eventex.subscriptions',
+    'eventex.subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
